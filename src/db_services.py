3a278c5887f5d8a98a787d344a62384f8ff2d421
Scareev <heitorlegobirds@gmail.com>
2026-08-27 15:15:27 -0300

    <chore>: merge das alterações do suzuki em relação ao banco de dados, aumento de complexidade informacional

--- src/db_services.py
+++ src/db_services.py
@@ -18,22 +18,29 @@ DB_PATH = os.path.join(BASE_DIR, "lojas.db")
 def db_path():
   return DB_PATH
 
-
 def create_db():
   conn = sqlite3.connect(DB_PATH)
   c = conn.cursor()
 
   # Create tables
   c.execute("""CREATE TABLE IF NOT EXISTS produtos (
-                nome TEXT, 
-                departamento TEXT
+                produto TEXT, 
+                departamento TEXT,
+                id INT,
+                data_fabri DATE,
+                data_venc DATE,
+                custos FLOAT,
+                fornecedor TEXT
             )""")
 
-  c.executemany("INSERT INTO produtos VALUES (?, ?)", [
-    ("sabonete", "higiene"),
-    ("agua", "bebidas"),
-    ("coca", "bebidas"),
+  c.executemany("INSERT INTO produtos VALUES (?, ?, ?, ?, ?, ?, ?)", [
+    ("sabonete", "higiene", 123, "2026-04-21", "2027-08-21", 23.50, "john"),
+    ("agua", "bebidas", 234, "2026-04-22", "2029-08-22", 3.50, "lennon"),
+    ("coca", "bebidas", 123, "2026-05-03", "2027-04-12", 7.00, "WOAH")
   ])
+
+  conn.commit()
+  conn.close()
 
   conn.commit()
   conn.close()
